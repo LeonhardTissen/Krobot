a7a2851014755f09e26e3575d6d1cb6ad81e9127
Leonhard Tissen <contact@warze.org>
2024-03-16 16:33:49 +0100

    Changed date specifics to be an object with arrays

--- src/dateSpecifics.py
+++ src/dateSpecifics.py
@@ -1,13 +1,30 @@
+dateSpecifics = {
+	"Sunday": [
+		":tv: The Queen of Sauce",
+		":cactus: Staircases from the Desert Trader for 1 Jade",
+		":gift: The gift limit has been reset",
+	],
+	"Monday": [
+		":clipboard: New Special Orders available",
+	],
+	"Wednesday": [
+		":tv: The Queen of Sauce Re-run",
+	],
+	"Thursday": [
+		":candy: Magic Rock Candy from the Desert Trader for 3 Prismatic Shards",
+		":arrow_up: Deluxe Speed-Gro from the Oasis for 80g",
+	],
+	"Friday": [
+		":pickaxe: Clint's Blacksmith shop is closed",
+	],
+}
+
 def getDateSpecifics(date):
 	specifics = ""
 
 	# Add new lines for various events throughout the week
-	if date == "Sunday":
-		specifics += "\n:tv: The Queen of Sauce"
-		specifics += "\n:cactus: Buy Staircases from the Desert Trader"
-	elif date == "Wednesday":
-		specifics += "\n:tv: The Queen of Sauce Re-run"
-	elif date == "Thursday":
-		specifics += "\n:cactus: Buy Magic Rock Candy from the Desert Trader"
+	if date in dateSpecifics:
+		for event in dateSpecifics[date]:
+			specifics += "\n" + event
 
 	return specifics
