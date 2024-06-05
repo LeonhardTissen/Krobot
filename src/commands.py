0f7cf3cfea5afeb0e260d6d886c6b4b286c1d1f1
Leonhard Tissen <contact@warze.org>
2024-06-05 19:44:06 +0200

    Cleanup: Reduced command complexity

--- src/commands.py
+++ src/commands.py
@@ -3,50 +3,57 @@ from .currentDay import getCurrentDay
 from .seasonNames import seasonNames
 from .reminders import getReminders
 
+dayMultipliers = {
+	"day": 1,
+	"week": 7,
+	"month": 28,
+}
+
+minimumDays = 1
+maximumDays = 112
+async def execCommandInNDays(args, message):
+	addedNumber = int(args[1])
+	addedType = args[2]
+	reminder = " ".join(args[3:])
+	# Remove s if it's plural
+	if addedType[:-1] == "s":
+		addedType = addedType[:-1]
+
+	if addedType not in dayMultipliers:
+		await message.channel.send("Please enter a valid time type (`days`, `weeks`, `months`)")
+		return
+	
+	addedDays = addedNumber * dayMultipliers[addedType]
+
+	if addedDays < minimumDays or addedDays > maximumDays:
+		await message.channel.send(f"Please enter a valid number of days (1-{maximumDays})")
+		return
+
+	(day, seasonId, year) = getCurrentDay()
+
+	newDay = int(day) + addedDays
+
+	while newDay > 28:
+		newDay = newDay - 28
+		seasonId += 1
+		if seasonId > 3:
+			seasonId = 0
+			year += 1
+
+	addReminderToJson({
+		"day": newDay,
+		"seasonId": seasonId,
+		"year": year,
+		"reminder": reminder,
+		"initiator": message.author.id
+	})
+
+	await message.channel.send(f"Reminder set for {seasonNames[seasonId]} {newDay}, Year {year}: {reminder}")
+
 async def execCommand(args, message):
 	# Example: in 3 days Ancient fruit will be ready
 	command = args[0]
 	if command == "in":
-		addedType = args[2]
-		addedNumber = args[1]
-
-		if addedType == "days" or addedType == "day":
-			addedDays = int(addedNumber)
-		elif addedType == "weeks" or addedType == "week":
-			addedDays = int(addedNumber) * 7
-		elif addedType == "months" or addedType == "month":
-			addedDays = int(addedNumber) * 28
-		else:
-			await message.channel.send("Please enter a valid time type (`days`, `weeks`, `months`)")
-			return
-
-		maximumDays = 112
-
-		if addedDays < 1 or addedDays > maximumDays:
-			await message.channel.send(f"Please enter a valid number of days (1-{maximumDays})")
-			return
-
-		reminder = " ".join(args[3:])
-
-		(day, seasonId, year) = getCurrentDay()
-
-		newDay = int(day) + addedDays
-
-		while newDay > 28:
-			newDay = newDay - 28
-			seasonId += 1
-			if seasonId > 3:
-				seasonId = 0
-				year += 1
-
-		addReminderToJson({
-			"day": newDay,
-			"seasonId": seasonId,
-			"year": year,
-			"reminder": reminder,
-			"initiator": message.author.id
-		})
-
-		await message.channel.send(f"Reminder set for {seasonNames[seasonId]} {newDay}, Year {year}: {reminder}")
+		await execCommandInNDays(args, message)
 	elif command == "reminders":
 		await message.channel.send(getReminders())
--- src/festivals.py
+++ src/festivals.py
@@ -1,39 +1,38 @@
 from .seasonNames import seasonNames
 
-def getFestival(day, seasonId):
-	festival = ""
-	season = seasonNames[seasonId]
-	if season == "Spring":
-		if day == 13:
-			festival = ":egg: Egg Festival"
-		elif day >= 15 and day <= 17:
-			festival = ":cactus: Desert Festival"
-		elif day == 24:
-			festival = ":tulip: Flower Dance"
-	elif season == "Summer":
-		if day == 11:
-			festival = ":palm_tree: Luau"
-		elif day >= 20 and day <= 21:
-			festival = ":fish: Trout Derby"
-		elif day == 28:
-			festival = ":jellyfish: Dance of the Moonlight Jellies"
-	elif season == "Fall":
-		if day == 16:
-			festival = ":flags: Stardew Valley Fair"
-		elif day == 27:
-			festival = ":jack_o_lantern: Spirit's Eve"
-	elif season == "Winter":
-		if day == 8:
-			festival = ":ice_cube: Festival of Ice"
-		elif day >= 12 and day <= 13:
-			festival = ":squid: SquidFest"
-		elif day >= 15 and day <= 17:
-			festival = ":ring_buoy: Night Market"
-		elif day == 25:
-			festival = ":glowing_star: Feast of the Winter Star"
+festivalList = {
+	"Spring": {
+		13: ":egg: Egg Festival",
+		15: ":cactus: Desert Festival",
+		16: ":cactus: Desert Festival",
+		17: ":cactus: Desert Festival",
+		24: ":tulip: Flower Dance"
+	},
+	"Summer": {
+		11: ":palm_tree: Luau",
+		20: ":fish: Trout Derby",
+		21: ":fish: Trout Derby",
+		28: ":jellyfish: Dance of the Moonlight Jellies"
+	},
+	"Fall": {
+		16: ":flags: Stardew Valley Fair",
+		27: ":jack_o_lantern: Spirit's Eve"
+	},
+	"Winter": {
+		8: ":ice_cube: Festival of Ice",
+		12: ":squid: SquidFest",
+		13: ":squid: SquidFest",
+		15: ":ring_buoy: Night Market",
+		16: ":ring_buoy: Night Market",
+		17: ":ring_buoy: Night Market",
+		25: ":glowing_star: Feast of the Winter Star"
+	}
+}
 
-	if festival != "":
-		return f"\n({festival})"
+def getFestival(day, seasonId):
+	season = seasonNames[seasonId]
+	festivalsInSeason = festivalList[season]
+	if day in festivalsInSeason:
+		return festivalsInSeason[day]
 	
-	# If no festival, return empty string
 	return ""
